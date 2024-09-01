e02d3a65f114ba0d2d0e421b1abf9c3ded6802ab
Gaurav Tatpate <152495952+Gaurav-Gt01@users.noreply.github.com>
2024-09-01 16:19:38 +0530

    Update blackjack.py

--- toy_text/blackjack.py
+++ toy_text/blackjack.py
@@ -1,86 +1,99 @@
-import gymnasium as gym
+import gym
 import numpy as np
 
-env = gym.make("Blackjack-v1")
-
 # Parameters
-gamma = 1.0
-numberofepisodes = 10000  
-epsilon = 1.0  
-epsilon_decay = 0.999 
-min_epsilon = 0.01 
+episodes = 10000  
+epsilonn = 0.1  
 
-actions = env.action_space.n
-Q = {}
+env = gym.make('Blackjack-v1', render_mode='rgb_array', sab=True, new_step_api=True)
 
-def choose_action(state):
-    if state not in Q:
-        Q[state] = np.zeros(actions)
-    if np.random.random() < epsilon:
-        return env.action_space.sample() 
+Q_table = np.zeros((18, 10, 2))
+
+def policy(playersum, dealercard, epsilon=epsilonn):
+    if np.random.rand() < epsilon:
+        return np.random.choice([0, 1])  
     else:
-        return np.argmax(Q[state])
+        return np.argmax(Q_table[playersum, dealercard]) 
 
-def generate_episode(env):
-    episode = []
-    state, _ = env.reset()
-    done = False
+# Monte Carlo Loop
+for episode in range(episodes):  
 
-    while not done:
-        action = choose_action(state)
-        next_state, reward, done, _, _ = env.step(action)
-        episode.append((state, action, reward))
+    state = env.reset()
+    
+    if isinstance(state, tuple):
+        state = state[0]
+    
+    episode_data = []
+    
+    terminated = False
+    truncated = False
+    
+    while not (terminated or truncated):
+        
+        if isinstance(state, tuple):
+            player_sum, dealer_card, _ = state
+        else:
+            player_sum, dealer_card, _ = (state, state, False)
+
+        # Storing the values according to the indexing in the Q-table 
+        playersum = player_sum - 4
+        dealercard = dealer_card - 1
+        
+        dealercard = min(max(dealercard, 0), 9)
+        
+        action = policy(playersum, dealercard)
+        
+        next_state, reward, terminated, truncated, _ = env.step(action)
+        
+        episode_data.append((playersum, dealercard, action, reward))
+        
         state = next_state
     
-    return episode
-
-# monte carlo 
-def monte_carlo_control():
-    returns_sum = {}
-    returns_count = {}
-    global epsilon
-
-    for numberofepisode in range(numberofepisodes):
-        episode = generate_episode(env)
-        G = 0
-        visited_state_action_pairs = set()
-
-        for state, action, reward in reversed(episode):
-            G = gamma * G + reward
-            if (state, action) not in visited_state_action_pairs:
-                visited_state_action_pairs.add((state, action))
-                if (state, action) not in Q:
-                    Q[state] = np.zeros(actions)
-                if (state, action) not in returns_sum:
-                    returns_sum[(state, action)] = 0.0
-                    returns_count[(state, action)] = 0.0
-                returns_sum[(state, action)] += G
-                returns_count[(state, action)] += 1.0
-                Q[state][action] = returns_sum[(state, action)] / returns_count[(state, action)]
-
-        
-        epsilon = max(min_epsilon, epsilon * epsilon_decay)
+    # Averaging the reward:
     
-    print("Training complete.")
+    G = 0
+    for playersum, dealercard, action, reward in reversed(episode_data):
+        G += reward
+        Q_table[playersum, dealercard, action] += (G - Q_table[playersum, dealercard, action]) / (episode + 1)
 
-# Train the model using Monte Carlo 
-monte_carlo_control()
+# Save the Q-table 
+np.save('qtable_blackjack.npy', Q_table)
 
-def test(env):
-    for _ in range(20):
-        state, _ = env.reset()
-        done = False
-        while not done:
-            action = np.argmax(Q.get(state, np.zeros(actions)))
-            next_state, reward, done, _, _ = env.step(action)
+Q_table_loaded = np.load('qtable_blackjack.npy')
 
-            if done:
-                print(f"Final State: {state}, Reward: {reward}")
-                break
-            state = next_state
+# Running the trained model:
 
-test_env = gym.make("Blackjack-v1", render_mode="human")
-test(test_env)
+env = gym.make('Blackjack-v1', render_mode='human', sab=True, new_step_api=True)  
+trainingepisodes = int(10)
 
-env.close()
-test_env.close()
+for episode in range(trainingepisodes):  
+    state = env.reset()
+    
+    terminated, truncated = False, False
+    
+    while not (terminated or truncated):
+        
+        if isinstance(state, tuple):
+            player_sum, dealer_card, _ = state
+        else:
+            player_sum, dealer_card, _ = (state, state, False)
+        
+        playersum = player_sum - 4
+        dealercard = dealer_card - 1
+        
+        dealercard = min(max(dealercard, 0), 9)
+        
+        action = np.argmax(Q_table_loaded[playersum, dealercard])
+        
+        state, reward, terminated, truncated, _ = env.step(action)
+        
+        if terminated or truncated:
+            if reward > 0:
+                result = "Win"
+            elif reward < 0:
+                result = "Loss"
+            else:
+                result = "Draw"
+            
+            print(f"Player's card sum is: {player_sum}, Dealer's card is: {dealer_card + 1}, Result is: {result}")
+
